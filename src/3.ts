49995817512eaac9c8d8581c04fc367ba3e19632
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-11-01 09:22:54 +0200

    com 2

--- src/3.ts
+++ src/3.ts
@@ -8,6 +8,14 @@
 */
 
 // реалізація класу Wizard
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+}
+interface ISpellCaster {
+  castSpell(): void;
+}
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
