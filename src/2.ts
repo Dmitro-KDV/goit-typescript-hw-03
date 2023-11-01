272bab03fbf3ef706ad981834588a9bb0baa0cb6
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-11-01 12:19:07 +0200

    com 1

--- src/2.ts
+++ src/2.ts
@@ -30,11 +30,10 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor(salary: number) {
-    super(salary);
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
 
   }
-  this.salary += 10000
 }
 
 
--- src/4.ts
+++ src/4.ts
@@ -1,6 +1,56 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+
+    constructor(door: boolean, key: Key) {
+        this.door = door;
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person)
+        }   
+    }
+    public abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('The door is now open.');
+        } else {
+            console.log('Invalid key. Cannot open the door.');
+        }
+    }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse(false, key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
